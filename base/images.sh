4eaa9cba7517f691efcc525f301d53df645488ec
suqingshan <suqingshan@suitbim.com>
2023-06-14 22:23:07 +0800

    修改base

--- base/images.sh
+++ base/images.sh
@@ -3,6 +3,7 @@ docker tag builder-base:v3.3.1-podman tomsue123/builder-base:v3.3.1-podman
 docker push tomsue123/builder-base:v3.3.1-podman
 
 docker build -t builder-base:v3.3.1 -f Dockerfile  .
+
 docker tag builder-base:v3.3.1 tomsue123/builder-base:v3.3.1
 docker push tomsue123/builder-base:v3.3.1
 
